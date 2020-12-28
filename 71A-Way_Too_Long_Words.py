e68668aa07f13993b8a3156f02323dbf354e28fc
Buğra <aldal.bugra@gmail.com>
2020-12-28 16:40:15 +0300

    Added explanatory comments

--- 71A-Way_Too_Long_Words.py
+++ 71A-Way_Too_Long_Words.py
@@ -1,12 +1,13 @@
 # https://codeforces.com/problemset/problem/71/A
-n = int(input())
+
+n = int(input()) # Get the lenght of the word.
 
 for i in range(0, n):
     x = str(input())
     l = len(x)
+    # if the lenght of the word is bigger than 10, get the first and the last letter
+    # and then mend it all together.
     if l > 10:
-        print(x[0], end="")
-        print(l-2, end="")
-        print(x[l-1])
+        print(f"{x[0]}{l-2}{x[l-1]}")
     else:
         print(x)
