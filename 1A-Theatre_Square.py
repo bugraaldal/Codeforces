46da2a351e2fe6d4c18f2872ebabdc22470c80fa
Buğra <61989756+bugraaldal@users.noreply.github.com>
2020-12-28 13:12:20 +0300

    Create 1A-Theatre_Square.py

---
+++ 1A-Theatre_Square.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/problemset/problem/1/A
+import math
+x = input()
+x = x.split()
+m = int(x[0])
+n = int(x[1])
+a = int(x[2])
+remainder1 = m/a
+remainder1 = math.ceil(remainder1)
+remainder2 = n/a
+remainder2 = math.ceil(remainder2)
+print(remainder2*remainder1)
