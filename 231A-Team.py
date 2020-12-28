0c6b9d899d3ff36c5b63e1271f42d228a8f08d69
Buğra <aldal.bugra@gmail.com>
2020-12-28 16:46:56 +0300

    Added explanatory comments

--- 231A-Team.py
+++ 231A-Team.py
@@ -1,4 +1,6 @@
 # https://codeforces.com/problemset/problem/231/A
+""" Getting the input and counting the 1s and 0s. 
+    If there are more ones than zeros, team should answer""""
 shoould_Answer = 0
 surness = int(input())
 for inputs in range(0, surness):
