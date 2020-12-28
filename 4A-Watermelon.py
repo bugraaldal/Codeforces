750a26ce19020fb3ff0a13b4675de72f81127598
Buğra <aldal.bugra@gmail.com>
2020-12-28 16:41:53 +0300

    Update 4A-Watermelon.py

--- 4A-Watermelon.py
+++ 4A-Watermelon.py
@@ -1,4 +1,6 @@
 # https://codeforces.com/problemset/problem/4/A
+""" In order to buy a watermelon that is sliceable to two even parts, we must buy a watermelan which has the weights of an even number.
+    Except the number 2 because it can only be divided 1/1. """
 w = int(input())
 if w % 2 == 0 and w != 2:
     print("YES")
