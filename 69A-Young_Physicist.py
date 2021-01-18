b0758cae20946784d0ccd7b4fb3c874faaa3049d
Buğra <aldal.bugra@gmail.com>
2021-01-18 22:20:07 +0300

    Add files via upload

---
+++ 69A-Young_Physicist.py
@@ -0,0 +1,12 @@
+lines = int(input())
+xs, ys, zs = [], [], []
+for i in range(0, lines):
+    coords = input()
+    x, y, z = coords.split(" ")
+    xs.append(int(x))
+    ys.append(int(y))
+    zs.append(int(z))
+if sum(xs) == 0 and sum(ys) == 0 and sum(zs) == 0:
+    print("YES")
+else:
+    print("NO")
