b145db934a1b59748444e79a629acb843945587a
Buğra <aldal.bugra@gmail.com>
2021-01-19 16:26:14 +0300

    Create 41A-Translation.py

---
+++ 41A-Translation.py
@@ -0,0 +1,9 @@
+# https://codeforces.com/contest/41/problem/A
+word = str(input())
+translated = str(input())
+final = "".join([e for e in ([translated[i:i+1]
+                              for i in range(0, len(word))][::-1])])
+if final == word:
+    print("YES")
+else:
+    print("NO")
