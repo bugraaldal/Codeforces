3972f4954b58a6ea432d04827f81e13047210c84
Buğra <aldal.bugra@gmail.com>
2021-01-29 10:25:13 +0300

    Deleted a function and renamed

--- 281A - Word Capitalization.py
+++ 281A - Word Capitalization.py
@@ -1,9 +1,5 @@
 # https://codeforces.com/problemset/problem/281/A
-def split(word):
-    return list(word)
-
-
 capital_Input = input("")
-compare_List = split(capital_Input)
+compare_List = list(capital_Input)
 a = compare_List[0].upper()
 print(a+capital_Input[1:])
