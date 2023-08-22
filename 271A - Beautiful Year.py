96bc354d4e8ed69ae526a792d07193b2cbabaa77
bugraaldal <aldal.bugra@gmail.com>
2023-08-23 02:12:06 +0300

    Add 271A

---
+++ 271A - Beautiful Year.py
@@ -0,0 +1,14 @@
+year = int(input())
+counts = []
+while True:
+    year += 1
+    for digit in str(year):
+        counts.append(str(year).count(digit))
+    if max(counts) == 1:
+        print(year)
+        break
+    else:
+        counts.clear()
+
+            
+            
